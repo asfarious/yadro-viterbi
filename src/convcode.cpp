bf6abe72e4c4cb22c246353eb97d70a2fef44c96
Matvei Osiptsov <matvei.osiptsov@metalab.ifmo.ru>
2025-05-20 21:52:14 +0300

    added comments to arcane bit manipulation

--- src/convcode.cpp
+++ src/convcode.cpp
@@ -37,6 +37,7 @@ void impulse2transitionTable(codeInput *impulseResponse, int constraintLength, t
   trTable transitionMatrix = {};
   int inputSize = __builtin_popcount(max_input);
   int outputSize = __builtin_popcount(max_output);
+  // This assumes max_input and max_output to be of form 2^n-1 !!!
   
   fsmState maxState = 0;
   for(int j = 0; j < constraintLength - 1; j++) {
@@ -46,14 +47,23 @@ void impulse2transitionTable(codeInput *impulseResponse, int constraintLength, t
     i.e. for max_input = 11, K = 3
     maxState is 0 -> 11 -> 1111
   */
+  
   for(fsmState state = 0; state < maxState+1; state++) {
     for(codeInput input = 0; input < max_input + 1; input++) {
+      
       fsmState nextState = (state >> inputSize) + (input * (maxState + 1) >> inputSize);
+      /*
+	i.e., for constraint_length = 4, state ABC and input I the next state is IAB
+       */
+
       codeInput coderRegister = input * (maxState + 1) + state;
+      // for the previous example, the register will be IABC.
+      
       codeOutput output = 0;
       codeOutput nextBit = 0;
       for(int j = 0; j < outputSize; j++) {
 	nextBit = __builtin_popcount(impulseResponse[j] & coderRegister) % 2;
+	// popcount(x) % 2 xors all the bits of x (adds them modulo 2) 
 	output = (output << 1) + nextBit;
       }
       transitionMatrix.emplace(std::make_tuple(state, input), std::make_tuple(nextState, output));
