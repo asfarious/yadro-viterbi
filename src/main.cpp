185b5b0a781e8dc44dafab7d9056c1e45ba180b1
Matvei Osiptsov <matvei.osiptsov@metalab.ifmo.ru>
2025-05-21 08:25:49 +0300

    implemented sampling of random sequences to estimate bit error rate

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,59 @@
 #include <map>
 #include <tuple>
 #include <iostream>
+#include <vector>
+#include <bitset>
+#include <random>
 
-#include "fsm.hpp"
 #include "parameters.hpp"
+#include "convcode.hpp"
+#include "viterbi.hpp"
+#include "channel.hpp"
 
-// Dear recruiter, please have mercy for I was a hair-length close to actually putting all the pieces together
 int main() {
-  trTable trans;
-  trans.emplace(std::make_tuple(0, 0), std::make_tuple(1, 0));
-  trans.emplace(std::make_tuple(0, 1), std::make_tuple(1, 1));
-  trans.emplace(std::make_tuple(1, 0), std::make_tuple(0, 0));
-  trans.emplace(std::make_tuple(1, 1), std::make_tuple(1, 1));
-
-  FiniteStateMachine fsm(&trans, 0);
-
-  codeOutput result;
-    
-  if(fsm.tryUpdate(1, &result)) {
-    std::cout << static_cast<int>(result) << fsm.getState() << std::endl;
-  } else {
-    std::cout << "error." << std::endl;
-  }
   
+  trTable transitionMatrix = {};
+
+  transitionMatrix = impulse2transitionTable(impulseResponse, constraintLength); 
+  
+  std::minstd_rand sampleGenerator;
+  std::uniform_int_distribution<codeInput> sampleDistribution(0, max_input);
+  codeInput input[sample_size] = {};
+  Channel channel(0);
+
+  std::vector<codeOutput> code;
+  
+  double probability_step = 0.5/static_cast<double>(probability_granularity);
+
+  std::cout << "probability, bit error rate" << std::endl;
+  
+  for(double p = 0; p < 0.5; p = p + probability_step) {
+    ConvCoder coder(transitionMatrix);
+    ViterbiDecoder decoder(constraintLength, transitionMatrix);
+    channel.setProbability(p);
+
+    for(int j = 0; j < sample_size; j++) {
+      input[j] = static_cast<codeInput>(sampleDistribution(sampleGenerator));
+    }
+    if(!coder.encode(input, sample_size)) {
+      std::cout << "error.";
+      return 1;
+    } else {
+      code = coder.getOutput();
+    }
+    int codeLen = code.size();
+    codeOutput *noisyCode = channel.applyNoise(&code[0], codeLen);
+
+    decoder.decode(noisyCode, codeLen);
+    decoder.flushOutput();
+    std::vector<codeInput> decoded = decoder.getOutput();
+    int wrongBits = 0;
+    for(int j = 0; j < sample_size; j++) {
+      wrongBits = wrongBits + __builtin_popcount(decoded[j] ^ input[j]);
+    }
+    double ber = static_cast<double>(wrongBits)/(static_cast<double>(sample_size)*input_size);
+    std::cout << p << ", " << ber << std::endl;
+  }
+    
   return 0;
 }
--- src/parameters.hpp
+++ src/parameters.hpp
@@ -17,16 +17,20 @@ typedef unsigned char codeOutput;
 
 // code parameters
 const int input_size = 2;
-const int output_size = 4;
+const int output_size = 6;
 const int constraintLength = 3;
 const codeInput max_input = (1 << input_size) - 1;
 const codeOutput max_output = (1 << output_size) - 1;
-const codeInput impulseResponse[4] = {0b100010, 0b100001, 0b110011, 0b111111};
+const codeInput impulseResponse[6] = {0b101011, 0b111111, 0b001100, 0b010010, 0b100001, 0b001001};
 
 /*
   details of internal specification of the finite state machine implementing the convolution code
  */
 typedef unsigned int fsmState;
 typedef std::map<std::tuple<fsmState, codeInput>, std::tuple<fsmState, codeOutput>> trTable;
+
+// random sample parameters
+const int sample_size = 1024;
+const int probability_granularity = 100;
 
 #endif
